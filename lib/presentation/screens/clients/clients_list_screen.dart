37ed9b6259ef99cdf61107bcb3ca8b99264784ba
ashiquekp <ashiquekp101@gmail.com>
2025-11-15 09:01:33 +0530

    code changes

--- lib/presentation/screens/clients/clients_list_screen.dart
+++ lib/presentation/screens/clients/clients_list_screen.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: use_build_context_synchronously
-
 import 'package:client_demo/presentation/widgets/app_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
